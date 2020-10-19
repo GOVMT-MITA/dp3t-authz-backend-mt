afe20d409240e59869f21493206fae95d140f98e
Malcolm Spiteri <malcolm.spiteri@gov.mt>
2020-10-19 13:17:17 +0200

    Allow from localhost

--- dpppt-authz-backend/src/main/java/org/dpppt/malta/backend/sdk/authz/ws/controller/CovidCodesController.java
+++ dpppt-authz-backend/src/main/java/org/dpppt/malta/backend/sdk/authz/ws/controller/CovidCodesController.java
@@ -41,9 +41,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/v1")
 public class CovidCodesController {
 
